249e088d6b99d323a165fede57add4aa34ebff00
Pratik <pratik@etherealcovenant.com>
2023-10-23 00:51:21 +0530

    Added serializers and migrations

--- app/models.py
+++ app/models.py
@@ -6,3 +6,4 @@ class Todo(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
